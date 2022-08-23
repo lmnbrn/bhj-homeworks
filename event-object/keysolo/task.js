5afe492b3463717a0eaa1332178ec1a9bb3b476b
lmnbrn <limeinbrain@gmail.com>
2022-08-23 13:46:07 +0300

    add keysolo task solution

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener("keyup", (event) => {
+      if (this.currentSymbol.textContent.toLowerCase() === event.key.toLowerCase()) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
@@ -87,4 +87,3 @@ class Game {
 }
 
 new Game(document.getElementById('game'))
-
