4e2f718c40f20bbc805aa11a14800af73feeb6a1
lmnbrn <limeinbrain@gmail.com>
2022-08-26 14:27:47 +0300

    add todo task solution

---
+++ document-structure/todo/task.js
@@ -0,0 +1,25 @@
+const taskInput = document.getElementById('task__input');
+const tasksList = document.getElementById('tasks__list');
+const tasksAdd = document.getElementById('tasks__add');
+tasksAdd.addEventListener('click', addTask);
+function removeTask() {
+    this.closest('.task').remove();
+}
+
+function addTask(event) {
+    if (taskInput.value !== '' && (event.code === 'Enter' || event.type === 'click')) {
+        const task = document.createElement('div');
+        task.classList.add('task');
+        task.innerHTML = `<div class='task__title'>
+      ${taskInput.value}
+      </div>
+      <a href='#' class='task__remove'>&times;</a>`;
+        tasksList.appendChild(task);
+        taskInput.value = '';
+        task.querySelector('.task__remove').addEventListener('click', removeTask);
+    }
+}
+
+function removeTask() {
+    this.closest('.task').remove();
+}
