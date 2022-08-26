59bc1264d4ec19deb64c67af80a8c412e78a8129
lmnbrn <limeinbrain@gmail.com>
2022-08-26 13:17:41 +0300

    add interests task solution (2nd attempt)

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,21 +1,37 @@
-const interestCheck = document.querySelectorAll(".interest__check");
-
-for (let item of interestCheck) {
-    const subItems = item.closest(".interest").querySelectorAll(".interest__check");
-    item.addEventListener('change', () => {
-        if (item.checked) {
-            if (subItems) {
-                for (let subItem of subItems) {
-                    subItem.checked = true;
+const clickAction = e => {
+    Array.from(
+        e.target.closest('li').getElementsByClassName('interest__check')
+    ).forEach(checkBox => {
+        checkBox.checked = e.target.checked;
+        checkBox.indeterminate = false;
+    });
+    const checkCategory = checkBox => {
+        let parent = checkBox.parentElement.closest('li');
+        let itemCheckbox = checkBox.parentElement.firstElementChild.firstElementChild.firstElementChild;
+        if (parent) {
+            let checked = itemCheckbox.checked;
+            let checkIndeterminate = itemCheckbox.indeterminate;
+            Array.from(checkBox.parentElement.children).forEach(item => {
+                let checkboxItem = item.firstElementChild.firstElementChild;
+                if (checkboxItem.checked != checked || checkboxItem.indeterminate) {
+                    checkIndeterminate = true;
                 }
+            });
+            let parentCheckbox = parent.firstElementChild.firstElementChild;
+            if (checkIndeterminate) {
+                parentCheckbox.indeterminate = true;
+            } else {
+                parentCheckbox.indeterminate = false;
+                parentCheckbox.checked = checked;
             }
-        } else {
-            if (subItems) {
-                for (let subItem of subItems) {
-                    subItem.checked = false;
-                }
-            }
+            checkCategory(parent);
         }
+    };
+    checkCategory(e.target.closest('li'));
+};
+
+Array.from(document.getElementsByClassName('interest__check')).forEach(
+    checkBox => {
+        checkBox.addEventListener('click', clickAction, false);
     }
-    );
-}
+);
